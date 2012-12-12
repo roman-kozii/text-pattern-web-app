roman-kozii/text-pattern-web-app

commit 9553f5ef8938625269b7a6a4c9806fcd512e8d96

Use function span in [edit] type links
git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4826 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/include/txp_file.php
@@ -270,8 +270,13 @@
 					, '', 'multi-edit').
 
 					hCell(
-						($can_edit ? href($id, $edit_url, ' title="'.gTxt('edit').'"') : $id).
-						(($file_exists) ? sp.'<span class="files_detail"><span role="presentation">[</span>'.make_download_link($id, gTxt('download'), $filename).'<span role="presentation">]</span></span>' : '')
+						($can_edit ? href($id, $edit_url, array('title' => gTxt('edit'))) : $id).
+						(($file_exists)
+							? sp.span('[', array('role' => 'presentation')).
+								make_download_link($id, gTxt('download'), $filename).
+								span(']', array('role' => 'presentation'))
+							: ''
+						)
 					, '', ' scope="row" class="id"').
 
 					td(

textpattern/include/txp_article.php
@@ -1763,8 +1763,8 @@
 	function article_partial_body($rs)
 	{
 		$out = graf('<label for="body">'.gTxt('body').'</label>'.popHelp('body').br.
-			n.'<textarea id="body" name="Body" cols="'.INPUT_LARGE.'" rows="'.TEXTAREA_HEIGHT_LARGE.'">'.txpspecialchars($rs['Body']).'</textarea>'
-		, ' class="body"');
+			n.'<textarea id="body" name="Body" cols="'.INPUT_LARGE.'" rows="'.TEXTAREA_HEIGHT_LARGE.'">'.txpspecialchars($rs['Body']).'</textarea>', ' class="body"'
+		);
 
 		return pluggable_ui('article_ui', 'body', $out, $rs);
 	}
@@ -1886,9 +1886,17 @@
 
 	function article_partial_section($rs)
 	{
-		$out = graf('<label for="section">'.gTxt('section').'</label> '.
-				'<span class="section-edit"><span role="presentation">[</span>'.eLink('section', '', '', '', gTxt('edit')).'<span role="presentation">]</span></span>'.br.
-				section_popup($rs['Section'], 'section'), ' class="section"');
+		$out = graf(
+			'<label for="section">'.gTxt('section').'</label>'.
+
+			sp.span(
+				span('[', array('role' => 'presentation')).
+				eLink('section', '', '', '', gTxt('edit')).
+				span(']', array('role' => 'presentation'))
+			, array('class' => 'section-edit')).br.
+
+			section_popup($rs['Section'], 'section')
+		, ' class="section"');
 
 		return pluggable_ui('article_ui', 'section', $out, $rs);
 	}
@@ -1906,12 +1914,24 @@
 	function article_partial_categories($rs)
 	{
 		$out = n.'<div id="categories_group">'.
-			graf('<label for="category-1">'.gTxt('category1').'</label> '.
-			'<span class="category-edit"><span role="presentation">[</span>'.eLink('category', '', '', '', gTxt('edit')).'<span role="presentation">]</span></span>'.br.
-			category_popup('Category1', $rs['Category1'], 'category-1'), ' class="category category-1"').
 
-			graf('<label for="category-2">'.gTxt('category2').'</label>'.br.
-			category_popup('Category2', $rs['Category2'], 'category-2'), ' class="category category-2"').
+			graf(
+				'<label for="category-1">'.gTxt('category1').'</label>'.
+
+				sp.span(
+					span('[', array('role' => 'presentation')).
+					eLink('category', '', '', '', gTxt('edit')).
+					span(']', array('role' => 'presentation'))
+				, array('class' => 'category-edit')).br.
+
+				category_popup('Category1', $rs['Category1'], 'category-1')
+			, ' class="category category-1"').
+
+			graf(
+				'<label for="category-2">'.gTxt('category2').'</label>'.br.
+				category_popup('Category2', $rs['Category2'], 'category-2')
+			, ' class="category category-2"').
+
 			n.'</div>';
 
 		return pluggable_ui('article_ui', 'categories', $out, $rs);

textpattern/include/txp_discuss.php
@@ -420,14 +420,16 @@
 				$ban_text = gTxt('ban');
 			}
 
-			$ban_link = '<span role="presentation">[</span><a class="action-ban" href="?event=discuss'.a.'step='.$ban_step.a.'ip='.$ip.
-				a.'name='.urlencode($name).a.'discussid='.$discussid.a.'_txp_token='.form_token().'">'.$ban_text.'</a><span role="presentation">]</span>';
+			$ban_link = sp.span('[', array('role' => 'presentation')).
+				'<a class="action-ban" href="?event=discuss'.a.'step='.$ban_step.a.'ip='.$ip.
+				a.'name='.urlencode($name).a.'discussid='.$discussid.a.'_txp_token='.form_token().'">'.$ban_text.'</a>'.
+				span(']', array('role' => 'presentation'));
 
 			$status_list = selectInput(
 				'visible',
 				array(
-					VISIBLE	 => gTxt('visible'),
-					SPAM		 => gTxt('spam'),
+					VISIBLE  => gTxt('visible'),
+					SPAM     => gTxt('spam'),
 					MODERATE => gTxt('unmoderated')
 				),
 				$visible,
@@ -441,7 +443,7 @@
 					hed(gTxt('edit_comment'), 2).
 					inputLabel('status', $status_list, 'status').
 					inputLabel('name', fInput('text', 'name', $name, '', '', '', INPUT_REGULAR, '', 'name'), 'name').
-					inputLabel('IP', $ip.n.$ban_link, '').
+					inputLabel('IP', $ip.$ban_link, '').
 					inputLabel('email', fInput('email', 'email', $email, '', '', '', INPUT_REGULAR, '', 'email'), 'email').
 					inputLabel('website', fInput('text', 'web', $web, '', '', '', INPUT_REGULAR, '', 'website'), 'website').
 					inputLabel('date', safe_strftime('%d %b %Y %X', $uPosted), '').
@@ -570,7 +572,10 @@
 
 					td(
 						txpspecialchars($ip).
-						n.'<span role="presentation">[</span><a class="action-ban" href="?event=discuss'.a.'step=ipban_unban'.a.'ip='.txpspecialchars($ip).a.'_txp_token='.form_token().'">'.gTxt('unban').'</a><span role="presentation">]</span>'
+						sp.span('[', array('role' => 'presentation')).
+						'<a class="action-ban" href="?event=discuss'.a.'step=ipban_unban'.a.'ip='.txpspecialchars($ip).
+						a.'_txp_token='.form_token().'">'.gTxt('unban').'</a>'.
+						span(']', array('role' => 'presentation'))
 					, '', 'ip').
 
 					td(

textpattern/include/txp_image.php
@@ -264,8 +264,12 @@
 					, '', 'multi-edit').
 
 					hCell(
-						($can_edit ? href($id, $edit_url, ' title="'.gTxt('edit').'"') : $id).sp.
-						'<span class="images_detail"><span role="presentation">[</span><a href="'.imagesrcurl($id, $ext).'">'.gTxt('view').'</a><span role="presentation">]</span></span>'
+						($can_edit ? href($id, $edit_url, array('title' => gTxt('edit'))) : $id).
+						sp.span(
+							span('[', array('role' => 'presentation')).
+							href(gTxt('view'), imagesrcurl($id, $ext)).
+							span(']', array('role' => 'presentation'))
+						, array('class' => 'images_detail'))
 					, '', ' scope="row" class="id"').
 
 					td(

textpattern/include/txp_link.php
@@ -321,7 +321,15 @@
 				inputLabel('linkname', fInput('text', 'linkname', $linkname, '', '', '', INPUT_REGULAR, '', 'linkname'), 'title').
 				inputLabel('linksort', fInput('text', 'linksort', $linksort, '', '', '', INPUT_REGULAR, '', 'linksort'), 'sort_value', 'link_sort').
 				inputLabel('url', fInput('text', 'url', $url, '', '', '', INPUT_REGULAR, '', 'url'), 'url', 'link_url', 'edit-link-url'). /* TODO: maybe use type = 'url' once browsers are less strict */
-				inputLabel('link_category', linkcategory_popup($category).' <span role="presentation">[</span>'.eLink('category', 'list', '', '', gTxt('edit')).'<span role="presentation">]</span>', 'link_category', 'link_category').
+
+				inputLabel(
+					'link_category',
+					linkcategory_popup($category).
+					sp.span('[', array('role' => 'presentation')).
+					eLink('category', 'list', '', '', gTxt('edit')).
+					span(']', array('role' => 'presentation'))
+				, 'link_category', 'link_category').
+
 				inputLabel('link_description', '<textarea id="link_description" name="description" cols="'.INPUT_LARGE.'" rows="'.TEXTAREA_HEIGHT_MEDIUM.'">'.txpspecialchars($description).'</textarea>', 'description', 'link_description', '', '').
 				pluggable_ui('link_ui', 'extend_detail_form', '', $rs).
 				graf(fInput('submit', '', gTxt('save'), 'publish')).

textpattern/include/txp_section.php
@@ -240,7 +240,15 @@
 						fInput('checkbox', 'selected[]', $sec_name)
 					, '', 'multi-edit').
 
-					hCell('<a href="'.$edit_url.'" title="'.gTxt('edit').'">'.$sec_name.'</a>' .n. '<span class="section_detail"><span role="presentation">[</span><a href="'.pagelinkurl(array('s' => $sec_name)).'">'.gTxt('view').'</a><span role="presentation">]</span></span>', '', ' scope="row" class="name"').
+					hCell(
+						href($sec_name, $edit_url, array('title' => gTxt('edit'))).
+						sp.span(
+							span('[', array('role' => 'presentation')).
+							href(gTxt('view'), pagelinkurl(array('s' => $sec_name))).
+							span(']', array('role' => 'presentation'))
+						, array('class' => 'section_detail'))
+					, '', ' scope="row" class="name"').
+
 					td(txpspecialchars($sec_title), '', 'title').
 					td('<a href="'.$page_url.'" title="'.gTxt('edit').'">'.$sec_page.'</a>', '', 'page').
 					td('<a href="'.$style_url.'" title="'.gTxt('edit').'">'.$sec_css.'</a>', '', 'style').

commit 505bce2ba70f9c2c9db530c7b4357259aa46fbd2

Sanitize HTTP values present in Files' edit links.
Also fixes the link's double encoding error introduced in
r4826.

git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5009 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/include/txp_file.php
@@ -243,8 +243,16 @@
 				extract($a);
 				$filename = sanitizeForFile($filename);
 
-				$edit_url = '?event=file'.a.'step=file_edit'.a.'id='.$id.a.'sort='.$sort.
-					a.'dir='.$dir.a.'page='.$page.a.'search_method='.$search_method.a.'crit='.$crit;
+				$edit_url = array(
+					'event'         => 'file',
+					'step'          => 'file_edit',
+					'id'            => $id,
+					'sort'          => $sort,
+					'dir'           => $dir,
+					'page'          => $page,
+					'search_method' => $search_method,
+					'crit'          => $crit,
+				);
 
 				$file_exists = file_exists(build_file_path($file_base_path, $filename));
 