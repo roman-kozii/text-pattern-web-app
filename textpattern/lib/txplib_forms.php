roman-kozii/text-pattern-web-app

commit 24d25ca0ac499e9ec3ef4b00434a1a6621d08eac

text_area() properly sanitizes HTML attributes.
git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5137 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/lib/txplib_forms.php
@@ -473,16 +473,47 @@
  * @return string HTML
  */
 
-	function text_area($name, $h = 0, $w = 0, $thing = '', $id = '', $rows = 5, $cols = 40, $placeholder='')
+	function text_area($name, $h = 0, $w = 0, $thing = '', $id = '', $rows = 5, $cols = 40, $placeholder = '')
 	{
-		$id = ($id) ? ' id="'.$id.'"' : '';
-		$rows = ' rows="' . ( ($rows && is_numeric($rows)) ? $rows : '4') . '"';
-		$cols = ' cols="' . ( ($cols && is_numeric($cols)) ? $cols : '48') . '"';
-		$width = ($w) ? 'width:'.$w.'px;' : '';
-		$height = ($h) ? 'height:'.$h.'px;' : '';
-		$style = ($width || $height) ? ' style="'.$width.$height.'"' : '';
+		$style = '';
 
-		return '<textarea'.$id.' name="'.$name.'"'.$rows.$cols.$style.($placeholder == '' ? '' : ' placeholder="'.txpspecialchars($placeholder).'"').'>'.txpspecialchars($thing).'</textarea>';
+		if ($w)
+		{
+			$style .= 'width:'.intval($w).'px;';
+		}
+
+		if ($h)
+		{
+			$style .= 'height:'.intval($h).'px;';
+		}
+
+		if ($thing === '')
+		{
+			$thing = null;
+		}
+		else
+		{
+			$thing = txpspecialchars($thing);
+		}
+
+		if (!intval($rows))
+		{
+			$rows = 5;
+		}
+
+		if (!intval($cols))
+		{
+			$cols = 40;
+		}
+
+		return tag($thing, 'textarea', array(
+			'name'        => $name,
+			'id'          => $id,
+			'rows'        => $rows,
+			'cols'        => $cols,
+			'style'       => $style,
+			'placeholder' => $placeholder,
+		));
 	}
 
 /**

commit 23a76d4ecac4e26f7a262e490528c383d3efdb9a

Render textareas even if the initial contents is NULL.
Fixes small regression from r5137.

git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5196 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/lib/txplib_forms.php
@@ -523,7 +523,7 @@
 			$style .= 'height:'.intval($h).'px;';
 		}
 
-		if ($thing === '')
+		if ((string) $thing === '')
 		{
 			$thing = null;
 		}