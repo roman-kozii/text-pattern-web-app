roman-kozii/text-pattern-web-app

commit 099860ce937e4d4514dd01806050d9dd730e6509

Use User API for updating user details.
git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4921 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/include/txp_admin.php
@@ -86,17 +86,18 @@
 
 	function author_save()
 	{
+		global $txp_user;
+
 		require_privs('admin.edit');
 
 		extract(doSlash(psa(array(
 			'privs',
-			'user_id',
+			'name',
 			'RealName',
 			'email',
 		))));
 
 		$privs = assert_int($privs);
-		$user_id = assert_int($user_id);
 
 		if (!is_valid_email($email))
 		{
@@ -104,14 +105,9 @@
 			return;
 		}
 
-		$rs = safe_update('txp_users', "
-			privs    = $privs,
-			RealName = '$RealName',
-			email    = '$email'",
-			"user_id = $user_id"
-		);
+		$rs = update_user($name, $email, $RealName);
 
-		if ($rs)
+		if ($rs && ($txp_user == $name || change_user_group($name, $privs)))
 		{
 			author_list(gTxt('author_updated', array('{name}' => $RealName)));
 			return;
@@ -546,6 +542,7 @@
 		if ($user_id)
 		{
 			$out[] = hInput('user_id', $user_id).
+				hInput('name', $name).
 				sInput('author_save');
 		}
 		else

commit 272b96e5b4092d699309678772cb6fec233d0ea0

In author_save(), do not escape user attributes  twice.
Fixes a regression from r4921.

git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5300 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/include/txp_admin.php
@@ -101,12 +101,12 @@
 
 		require_privs('admin.edit');
 
-		extract(doSlash(psa(array(
+		extract(psa(array(
 			'privs',
 			'name',
 			'RealName',
 			'email',
-		))));
+		)));
 
 		$privs = assert_int($privs);
 